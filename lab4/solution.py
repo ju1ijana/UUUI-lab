a8380bd534419717b15ea102c3f5f35300551cee
unknown <julijana.kolarec@fer.hr>
2024-05-31 14:17:55 +0200

    nn za sve arhitekture i sve dimenzije ulaznih vrijednosti

--- lab4/solution.py
+++ lab4/solution.py
@@ -20,17 +20,27 @@ def load_data(path):
     return data
 
 
+def get_weights(rows, columns):
+    return np.random.normal(0, 0.1, (rows, columns))
+
+
 def nn(dimension, data):
-    x_data = data['x']
-    if dimension == '5s' or dimension == '20s':
-        dimension = int(dimension[:-1])
-        for x in x_data:
-            weights = np.array([np.random.normal(0, 0.1) for _ in range(dimension)])
-            h = np.dot(weights, x) + np.array([np.random.normal(0, 0.1) for _ in range(dimension)])
+    columns = list(data.keys())[:-1]
+    layer_dimension = 5 if dimension in ['5s', '5s5s'] else 20
+    y_hat = []
+    for x in data[columns[0]] if len(columns) == 1 else zip(data[columns[0]], data[columns[1]]):
+        weights = get_weights(layer_dimension, len(columns))
+        x = np.array(x).reshape((2, 1)) if isinstance(x, tuple) else np.array([[x]])
+        h = np.dot(weights, x) + get_weights(layer_dimension, 1)
+        sigmoid = 1 / (1 + np.exp(-h))
+        if dimension == '5s5s':
+            weights = get_weights(layer_dimension, len(columns))
+            h = np.dot(weights, x) + get_weights(layer_dimension, 1)
             sigmoid = 1 / (1 + np.exp(-h))
-            weights = np.array([np.random.normal(0, 0.1) for _ in range(dimension)])
-            y_hat = np.dot(weights, sigmoid) + np.random.normal(0, 0.1)
-            y_hat = 1 / (1 + np.exp(-y_hat))
+        weights = get_weights(len(columns), layer_dimension)
+        y_hat.append((np.dot(weights, sigmoid) + get_weights(1, 1))[0][0])
+
+    err = (1/len(y_hat)) * sum((y - y_h)**2 for y, y_h in zip(data[list(data.keys())[-1]], y_hat))
 
 
 args = sys.argv[1:]
